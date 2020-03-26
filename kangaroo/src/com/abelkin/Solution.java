237ff094510b743c9201d2969538c6f89126c2c8
Andrew Belkin <andrew.belkin@gmail.com>
2020-03-26 18:26:03 +0300

    https://www.hackerrank.com/challenges/kangaroo/problem

---
+++ kangaroo/src/com/abelkin/Solution.java
@@ -0,0 +1,43 @@
+package com.abelkin;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the kangaroo function below.
+    private static String kangaroo(int x1, int v1, int x2, int v2) {
+        double step = (double)(x1 - x2) / (double)(v2 - v1);
+        return (step > 0 && Math.abs(Math.round(step) - step) < 0.0001d) ? "YES" : "NO";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("output.txt"));
+
+        String[] x1V1X2V2 = scanner.nextLine().split(" ");
+
+        int x1 = Integer.parseInt(x1V1X2V2[0]);
+
+        int v1 = Integer.parseInt(x1V1X2V2[1]);
+
+        int x2 = Integer.parseInt(x1V1X2V2[2]);
+
+        int v2 = Integer.parseInt(x1V1X2V2[3]);
+
+        String result = kangaroo(x1, v1, x2, v2);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
