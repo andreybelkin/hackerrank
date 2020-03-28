f3cc9aa1982c8d4d02b92d5db562fd742e9a9319
Andrey Belkin <abelkin@abelkin.com>
2020-03-28 21:14:35 +0300

    https://www.hackerrank.com/challenges/mini-max-sum/problem

---
+++ mini-max-sum/src/com/abelkin/Solution.java
@@ -0,0 +1,38 @@
+package com.abelkin;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] arr) {
+        long sum = Arrays.stream(arr).asLongStream().sum();
+        System.out.print(sum - Arrays.stream(arr).max().getAsInt());
+        System.out.print(" ");
+        System.out.print(sum - Arrays.stream(arr).min().getAsInt());
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        miniMaxSum(arr);
+
+        scanner.close();
+    }
+}
