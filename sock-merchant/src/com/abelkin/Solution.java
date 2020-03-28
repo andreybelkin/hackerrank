a537830f6874dd80828c7fb25f965179c6c38284
Andrey Belkin <abelkin@abelkin.com>
2020-03-28 18:47:47 +0300

    https://www.hackerrank.com/challenges/sock-merchant/problem

---
+++ sock-merchant/src/com/abelkin/Solution.java
@@ -0,0 +1,55 @@
+package com.abelkin;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the sockMerchant function below.
+    private static int sockMerchant(int n, int[] ar) {
+        Set<Integer> oneSock = new HashSet<>();
+        int pairs = 0;
+        for (int value : ar) {
+            if (oneSock.contains(value)) {
+                pairs++;
+                oneSock.remove(value);
+            } else {
+                oneSock.add(value);
+            }
+        }
+        return pairs;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("output.txt"));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[n];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
