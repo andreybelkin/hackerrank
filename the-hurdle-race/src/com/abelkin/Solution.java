35856f62f43f7a470a15cbeadb023b3cffcfac98
Andrey Belkin <abelkin@abelkin.com>
2020-03-29 05:22:14 +0300

    https://www.hackerrank.com/challenges/the-hurdle-race/problem

---
+++ the-hurdle-race/src/com/abelkin/Solution.java
@@ -0,0 +1,51 @@
+package com.abelkin;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    private static int hurdleRace(int k, int[] height) {
+        int max = 0;
+        for (int value : height) {
+            max = Math.max(max, value);
+        }
+        return Math.max(0, max-k);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("output.txt"));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] height = new int[n];
+
+        String[] heightItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int heightItem = Integer.parseInt(heightItems[i]);
+            height[i] = heightItem;
+        }
+
+        int result = hurdleRace(k, height);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
