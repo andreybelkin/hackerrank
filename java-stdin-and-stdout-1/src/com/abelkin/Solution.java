2d8d2d847487aa55d5c45401430115582e995fab
Andrew Belkin <andrew.belkin@gmail.com>
2020-03-30 16:15:40 +0300

    https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem

---
+++ java-stdin-and-stdout-1/src/com/abelkin/Solution.java
@@ -0,0 +1,15 @@
+package com.abelkin;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            int num = scanner.nextInt();
+            System.out.println(num);
+        }
+
+    }
+}
