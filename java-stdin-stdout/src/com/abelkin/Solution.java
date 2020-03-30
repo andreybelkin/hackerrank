a07e3fd28114eab3eb2c73d25181c554d990af71
Andrew Belkin <andrew.belkin@gmail.com>
2020-03-30 16:49:28 +0300

    https://www.hackerrank.com/challenges/java-stdin-stdout/problem

---
+++ java-stdin-stdout/src/com/abelkin/Solution.java
@@ -0,0 +1,19 @@
+package com.abelkin;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int integer = scanner.nextInt();
+        double doubl = scanner.nextDouble();
+        scanner.nextLine();
+        String string = scanner.nextLine();
+
+        System.out.println("String: " + string);
+        System.out.println("Double: " + doubl);
+        System.out.println("Int: " + integer);
+
+    }
+}
