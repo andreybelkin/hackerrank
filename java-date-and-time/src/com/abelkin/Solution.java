a98dcd7a861a2e7498ae71e04c93f78d9b7fd0cd
Andrew Belkin <andrew.belkin@gmail.com>
2020-03-31 17:21:12 +0300

    https://www.hackerrank.com/challenges/java-date-and-time/problem

---
+++ java-date-and-time/src/com/abelkin/Solution.java
@@ -0,0 +1,61 @@
+package com.abelkin;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    static String findDay(int month, int day, int year) {
+        Calendar cal = new GregorianCalendar(year,month-1,day);
+        switch (cal.get(Calendar.DAY_OF_WEEK)) {
+            case 2: return "MONDAY";
+            case 3: return "TUESDAY";
+            case 4: return "WEDNESDAY";
+            case 5: return "THURSDAY";
+            case 6: return "FRIDAY";
+            case 7: return "SATURDAY";
+            case 1: return "SUNDAY";
+        }
+        return "";
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("output.txt"));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
