9ecfd6014b9cc010956c3e5f9d7c9fb34692b048
Andrew Belkin <andrew.belkin@gmail.com>
2020-03-31 13:08:50 +0300

    https://www.hackerrank.com/challenges/java-loops-i/problem

---
+++ java-loops-i/src/com/abelkin/Solution.java
@@ -0,0 +1,15 @@
+package com.abelkin;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + n * i);
+        }
+
+    }
+}
