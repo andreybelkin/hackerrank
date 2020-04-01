da4232ec7cc4ab54a23cf334faa2cacd6bb200b0
Andrew Belkin <andrew.belkin@gmail.com>
2020-04-01 22:40:35 +0300

    https://www.hackerrank.com/challenges/java-substring/problem

---
+++ java-substring/src/com/abelkin/Solution.java
@@ -0,0 +1,14 @@
+package com.abelkin;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        System.out.println(s.substring(start, end));
+    }
+}
