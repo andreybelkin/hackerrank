4d289f264c56880816e739d40854f68c8dd2a861
Andrew Belkin <andrew.belkin@gmail.com>
2020-04-01 23:31:54 +0300

    https://www.hackerrank.com/challenges/java-string-tokens/problem

---
+++ java-string-tokens/src/com/abelkin/Solution.java
@@ -0,0 +1,19 @@
+package com.abelkin;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine().trim();
+        String[] strings = s.split("[ !,?._'@]+");
+        if (s.equals("")) {
+            System.out.println(0);;
+            return;
+        }
+        System.out.println(strings.length);
+        Arrays.stream(strings).forEach(System.out::println);
+        scan.close();
+    }
+}
