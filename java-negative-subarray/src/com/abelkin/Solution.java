c56b541196d0cd031719bc8d8f35383f20405fd9
Andrew Belkin <andrew.belkin@gmail.com>
2020-04-04 12:00:42 +0300

    https://www.hackerrank.com/challenges/java-negative-subarray/problem

---
+++ java-negative-subarray/src/com/abelkin/Solution.java
@@ -0,0 +1,24 @@
+package com.abelkin;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        int counter = 0;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += a[j];
+                counter += (sum < 0) ? 1 : 0;
+            }
+        }
+        System.out.println(counter);
+    }
+}
