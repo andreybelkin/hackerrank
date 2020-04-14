ab7f7e1f4ff74961c0687aa0c784b93eda2dd8bc
Andrew Belkin <andrew.belkin@gmail.com>
2020-04-14 20:12:41 +0300

    https://www.hackerrank.com/challenges/ctci-bubble-sort/problem

---
+++ ctci-bubble-sort/src/com/abelkin/Solution.java
@@ -0,0 +1,30 @@
+package com.abelkin;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        int swaps = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                if (a[j] > a[j+1]) {
+                    int change = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = change;
+                    swaps++;
+                }
+            }
+        }
+        System.out.println("Array is sorted in " + swaps + " swaps.");
+        System.out.println("First Element: " + a[0]);
+        System.out.println("Last Element: " + a[n-1]);
+
+    }
+}
