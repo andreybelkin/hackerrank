1b1a97065e46a2c486ed3305522ebe75e3ca5932
Andrew Belkin <andrew.belkin@gmail.com>
2020-05-10 15:31:50 +0300

    Optimization

--- array-left-rotation/src/com/abelkin/Solution.java
+++ array-left-rotation/src/com/abelkin/Solution.java
@@ -5,18 +5,11 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt(), d = scanner.nextInt();
-        int rest = (n - d) % n;
-        n -= rest;
-        ArrayList<Integer> a = new ArrayList<>();
-        while (n-- > 0) {
-            a.add(scanner.nextInt());
-        }
-        for (int i = 0; i < rest; i++) {
-            a.add(i, scanner.nextInt());
-        }
-        a.forEach(x -> System.out.print(x + " "));
+        int mask = 0x000F;
+        int value = 0x2222;
+        System.out.println(value & mask);
+        /* This is also a comment // More comments */
+        /* This is also a comment /* More comments */ */
 
     }
 }
